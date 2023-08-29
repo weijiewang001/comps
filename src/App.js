4e59a5f5850699b93912ce689b24407f5bbaffa8
wayne <weijiewang001@gmail.com>
2023-08-29 15:07:33 +1000

    Marking Navigation Reusable

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import { useState } from "react";
-import Dropdown from "./components/Dropdown";
+import Route from './components/Route';
+import AccordionPage from './pages/AccordionPages'
+import DropdownPage from './pages/DropdownPage'
+import Sidebar from './components/Sidebar';
+import ButtonPage from './pages/ButtonPage'
+
+
 
 function App() {
-  const [selection, setSelection] = useState(null);
-
-  const handleSelect = (option) => {
-    setSelection(option);
-  };
-  const options = [
-    { label: 'Red', value: 'red' },
-    { label: 'Green', value: 'green' },
-    { label: 'Blue', value: 'blue' },
-  ];
-
   return (
-    <div className="flex">
-      <Dropdown options={options} value={selection} onChange={handleSelect} />
-      <Dropdown options={options} value={selection} onChange={handleSelect} />
+    <div className='container mx-auto grid grid-cols-6 gap-4 mt-4'>
+      <Sidebar />
+      <div className='col-span-5'>
+        <Route path="/accordion">
+          <AccordionPage />
+        </Route>
+        <Route path="/">
+          <DropdownPage />
+        </Route>
+        <Route path="/buttons">
+          <ButtonPage />
+        </Route>
+      </div>
 
-    </div>
-  )
-
+    </div>);
 }
 
 export default App;
